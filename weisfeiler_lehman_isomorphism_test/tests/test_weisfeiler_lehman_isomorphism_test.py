fd88a8c9badc4d53e092bc3468601ab6ab16270d
matheus1714 <matheussilvamartins1714@gmail.com>
2021-08-28 10:14:17 -0300

    basics tests for class method WL

--- weisfeiler_lehman_isomorphism_test/tests/test_weisfeiler_lehman_isomorphism_test.py
+++ weisfeiler_lehman_isomorphism_test/tests/test_weisfeiler_lehman_isomorphism_test.py
@@ -1,6 +1,21 @@
 import pytest
 from src.model.WL import WL
+from igraph import *
 
-# Testar função com parâmetros de entrada vazios
 def test_function_with_empty_input_parameters():
-    pass
+    wl = WL()
+    with pytest.raises(TypeError):
+        wl.weisfeiler_lehman_isomorphism_test() is None
+
+def test_passing_only_one_of_the_graphs():
+    g = Graph()
+    wl = WL()
+    with pytest.raises(TypeError):
+        wl.weisfeiler_lehman_isomorphism_test(g) is None
+
+def test_passing_empty_parameters():
+    wl = WL()
+    g = Graph()
+    assert wl.weisfeiler_lehman_isomorphism_test(None, None) == -1
+    assert wl.weisfeiler_lehman_isomorphism_test(g, None) == -1
+    assert wl.weisfeiler_lehman_isomorphism_test(None, g) == -1
